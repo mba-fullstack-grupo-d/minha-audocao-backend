90a4a44c8c293107eda10c53a1f99dbc516ba576
Paolo Zilioti <pzilioti@gmail.com>
2021-07-16 21:16:06 -0300

    correção

--- src/main/java/br/com/minhaudocao/adote/service/InstituicaoService.java
+++ src/main/java/br/com/minhaudocao/adote/service/InstituicaoService.java
@@ -153,7 +153,7 @@ public class InstituicaoService {
 
         if(enderecos != null && search.getNome() != null){
             for(Endereco endereco: enderecos){
-                if(instituicaos != null){
+                if(instituicaos == null){
                     instituicaos = instituicaoRepository.findByNomeAndEndereco(search.getNome(), endereco);
                 }else {
                     instituicaos.addAll(instituicaoRepository.findByNomeAndEndereco(search.getNome(), endereco));
@@ -161,7 +161,7 @@ public class InstituicaoService {
             }
         }else if(enderecos != null){
             for(Endereco endereco: enderecos){
-                if(instituicaos != null){
+                if(instituicaos == null){
                     instituicaos = instituicaoRepository.findByEndereco(endereco);
                 }else {
                     instituicaos.addAll(instituicaoRepository.findByEndereco(endereco));
